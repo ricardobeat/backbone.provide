27f4373ea063b302abc480f68e3009fa563f094a
Ricardo Tomasi <ricardobeat@gmail.com>
2014-01-16 16:24:48 -0200

    emit response on requester by default

--- backbone.provide.js
+++ backbone.provide.js
@@ -13,11 +13,11 @@
 
 Backbone.Events.prototype.request = function (id, callback, context) {
     this.once(id, callback)
-    Backbone.trigger('request-' + id, context)
+    Backbone.trigger('request-' + id, context || this)
 }
 
 Backbone.Events.prototype.provide = function (id, getData) {
-    this.listenTo(Backbone, 'request-' + id, function(context){
+    this.listenTo(Backbone, 'request-' + id, function (context) {
         (context || Backbone).trigger(id, getData.call(this))
     })
 }
